fbd633b40bf815c41ed70ba2f9bfd9a26a19c67a
sulfred.lee <sulfred.lee@aminocom.com>
2018-07-26 18:16:12 +0800

    Add new example to show define array from file

---
+++ example/defArrayFromFile/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+char init_data[300] =
+    #include "key.h"
+
+int main(int argc, char* argv[]){
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << init_data[i] << std::endl;
+    }
+    return 0;
+}
