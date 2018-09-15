1f8eed803fb911872d1de05bd13329ca0e405c08
Sulfred Lee <sflee1112@gmail.com>
2018-09-15 08:06:21 +0800

    Added flag indicator

---
+++ other/qt5/WolfeWaves/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dstPath="/mnt/c/Users/sflee/AppData/Roaming/MetaQuotes/Terminal/D0E8209F77C8CF37AD8BF550E51FF075/MQL5"
+ProjectName="WolfeWaves"
+sourcePath="$PWD"
+
+# get input argument
+while getopts a:h: option
+do
+    case "${option}"
+    in
+        a) ACTION=${OPTARG};;
+        h) HELP=${OPTARG};;
+    esac
+done
+
+if [ -z $ACTION ]; then
+    echo "Usage: $0 -a [install|clean]"
+    exit
+fi
+
+if [ "${ACTION}" == "install" ]; then
+    cp -r "Experts/${ProjectName}" "${dstPath}/Experts"
+    cp -r "Indicators/${ProjectName}" "${dstPath}/Indicators"
+elif [ "${ACTION}" == "clean" ]; then
+    rm -rf "${dstPath}/Experts/${ProjectName}"
+    rm -rf "${dstPath}/Indicators/${ProjectName}"
+fi
