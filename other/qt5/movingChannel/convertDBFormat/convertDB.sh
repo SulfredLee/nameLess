1f4df97dedeebeab024cea41fc22f951dc7df7f0
sulfred.lee <sulfred.lee@aminocom.com>
2018-10-09 09:01:58 +0800

    move project to other places

--- other/qt5/movingChannel/convertDBFormat/convertDB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir convertDB
-inputFiles=($(ls *.csv))
-for inputFile in "${inputFiles[@]}"
-do
-    echo "${inputFile}"
-    iconv -f UCS-2LE -t UTF-8 ${inputFile} >> ./convertDB/${inputFile}
-done
--- other/qt5/rangeTraderIndicator/common.mqh
+++
@@ -1,5 +0,0 @@
-#property copyright "Copyright 2018, Damage Company"
-
-#define BL_LAYERS 4
-#define SL_LAYERS 4
-#define EXPERT_MAGIC 338866   // MagicNumber of the expert
