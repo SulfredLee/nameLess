de906d88c2ca1a8ebf81f520551c06c9833f6bc8
sulfred.lee <sulfred.lee@aminocom.com>
2018-11-06 10:38:05 +0800

    Can read element content and attribute

--- example/learnXML2_C/main.c
+++ example/learnXML2_C/main.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
+
 /**
  * print_element_names:
  * @a_node: the initial xml node to consider.
@@ -14,32 +15,79 @@
  * Prints the names of the all the xml elements
  * that are siblings or children of a given xml node.
  */
-static void
-print_element_names(xmlNodePtr a_node)
+static void print_element_names(xmlNodePtr a_node)
 {
     xmlNodePtr cur_node = NULL;
 
-    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
-        if (cur_node->type == XML_ELEMENT_NODE) {
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next)
+    {
+        if (cur_node->type == XML_ELEMENT_NODE && xmlStrcmp(cur_node->name, (xmlChar *)"image") == 0)
+        {
             printf("node type: Element, name: %s\n", cur_node->name);
+            if (cur_node->children)
+            {
+                printf("node type: Text, content: %s\n", cur_node->children->content);
+            }
+            xmlChar* uri;
+            uri = xmlGetProp(cur_node, (xmlChar*)"encodin");
+            printf("uri: %s\n", uri == NULL ? "NULLLLLL" : "Has");
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"imagetype");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"id");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
         }
-        if (cur_node->type == XML_TEXT_NODE) {
-            printf("node type: Text, content: %s, length: %d\n", cur_node->content, strlen(cur_node->content));
+        else if (cur_node->type == XML_ELEMENT_NODE && xmlStrcmp(cur_node->name, (xmlChar *)"region") == 0)
+        {
+            printf("node type: Element, name: %s\n", cur_node->name);
+            xmlChar* uri;
+            uri = xmlGetProp(cur_node, (xmlChar*)"id");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"extent");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"origin");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+        }
+        else if (cur_node->type == XML_ELEMENT_NODE && xmlStrcmp(cur_node->name, (xmlChar *)"div") == 0)
+        {
+            printf("node type: Element, name: %s\n", cur_node->name);
+            xmlChar* uri;
+            uri = xmlGetProp(cur_node, (xmlChar*)"begin");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"end");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"region");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"backgroundImage");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
+            uri = xmlGetProp(cur_node, (xmlChar*)"id");
+            printf("uri: %s\n", uri);
+            xmlFree(uri);
         }
 
         print_element_names(cur_node->children);
     }
 }
 
-int
-main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     char         *docname;
     xmlDocPtr    doc;
     xmlNodePtr   cur;
     xmlChar      *uri;
 
-    if (argc <= 1) {
+    if (argc <= 1)
+    {
         printf("Usage: %s docname\n", argv[0]);
         return(0);
     }
@@ -53,9 +101,11 @@ main(int argc, char **argv) {
     return 1;
 
     cur = cur->xmlChildrenNode;
-    while (cur != NULL) {
-        if ((!xmlStrcmp(cur->name, (const xmlChar *)"reference"))) {
-            uri = xmlGetProp(cur, "uri");
+    while (cur != NULL)
+    {
+        if ((!xmlStrcmp(cur->name, (const xmlChar *)"reference")))
+        {
+            uri = xmlGetProp(cur, (xmlChar *)"uri");
             printf("uri: %s\n", uri);
             xmlFree(uri);
         }
