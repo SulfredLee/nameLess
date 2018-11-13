247621b6ff6c5f560115dde077c785ce0a20e2df
sulfred.lee <sulfred.lee@aminocom.com>
2018-11-13 09:43:25 +0800

    use utility library for file reading

--- app/png2bmp/main.cpp
+++ app/png2bmp/main.cpp
@@ -6,25 +6,7 @@
 #include <string.h>
 
 #include "png2bmp.h"
-
-bool GetPNGFromFile(const std::string& pngFile, std::vector<unsigned char>& pngImage)
-{
-    // Reading size of file
-    FILE * file = fopen(pngFile.c_str(), "rb");
-    if (file == NULL) return false;
-    fseek(file, 0, SEEK_END);
-    long int size = ftell(file);
-    fclose(file);
-    // Reading data to array of unsigned chars
-    file = fopen(pngFile.c_str(), "rb");
-    pngImage.resize(size);
-    int bytes_read = fread((char*)&(pngImage[0]), sizeof(unsigned char), size, file);
-    fclose(file);
-
-    if (bytes_read != size) return false;
-
-    return true;
-}
+#include "Tools.h"
 
 int main(int argc, char* argv[])
 {
@@ -34,7 +16,7 @@ int main(int argc, char* argv[])
         return -1;
     }
     std::vector<unsigned char> pngImage;
-    if (!GetPNGFromFile(argv[1], pngImage))
+    if (!Utility::ReadFileToArray(argv[1], pngImage))
     {
         std::cout << "Cannot get png file to memory" << std::endl;
         return -1;
